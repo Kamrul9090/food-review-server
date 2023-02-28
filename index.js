44dc99a0df2cf693fb268f46dc8403e781a5c800
Kamrul Hasan <CR7.kamrul.9090@gmail.com>
2023-02-28 06:14:35 +0600

    make allfoods and category request done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, CURSOR_FLAGS } = require('mongodb');
 require('dotenv').config()
 
 const port = process.env.PORT || 5000;
@@ -8,6 +9,54 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = process.env.REACT_APP_uri;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+        const AllServicesCollection = client.db('FoodsReview').collection('AllServices');
+        const categoryCollection = client.db("FoodsReview").collection('CategoryFoods')
+
+        // Get request 
+        app.get('/AllServices', async (req, res) => {
+            const query = {};
+            const AllFoodsData = AllServicesCollection.find(query);
+            const result = await AllFoodsData.limit(3).toArray();
+            if (result) {
+                return res.send(result)
+            } else {
+                return res.send({ message: "Not found" });
+            }
+        })
+
+        app.get('/AllFoods', async (req, res) => {
+            const query = {};
+            const AllFoodsData = await AllServicesCollection.find(query).toArray();
+            if (AllFoodsData) {
+                return res.send(AllFoodsData)
+            } else {
+                return res.send({ message: "Data Not found" })
+            }
+        })
+
+        app.get('/foods', async (req, res) => {
+            const query = {};
+            const result = await categoryCollection.find(query).toArray();
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(e => console.error(e))
+
 
 app.get('/', (req, res) => {
     res.send('server open on port 5000');
